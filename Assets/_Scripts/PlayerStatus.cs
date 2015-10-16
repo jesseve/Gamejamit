c7172307c93f97c56a8d0a57c254cc1949518b52
Jagulars <sakari.lukkaroinen@hotmail.com>
2015-10-16 21:00:47 +0300

    Pohja

---
+++ Assets/_Scripts/PlayerStatus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatus : MonoBehaviour {
+
+    int wealth;
+    int favorKing;
+    int favorPeople;
+    int peace;
+    int military;
+    
+
+    void EndOfTurn()
+    {
+        peace = favorPeople + military;
+    }
+}
