e50304de1c2d569abe8986e5b4d6a69098079601
jesseve <jesseverainen@gmail.com>
2015-10-16 23:58:44 +0300

    vuoroskripti

--- Assets/_Scripts/TurnController.cs
+++ Assets/_Scripts/TurnController.cs
@@ -5,6 +5,64 @@ using System.Collections.Generic;
 
 public class TurnController : MonoBehaviour {
 
+	public List<CardScript> possibleCards;
+
+	public int playerCount = 3;
+
+	public List<King> kings;
+
+	private List<CardScript> cardsOnTable;
+
+	void Start() {
+		kings = new List<King>();
+		for(int i = 0; i < playerCount; i++) {
+			kings.Add(new King());
+		}
+
+		cardsOnTable = new List<CardScript>();
+		cardsOnTable.Add(possibleCards[0]);
+		cardsOnTable.Add(possibleCards[0]);
+		cardsOnTable.Add(possibleCards[0]);
+		StartTurn();
+	}
+
+	public void StartTurn() {
+		NewCards ();
+		foreach(King k in kings){
+			k.Upkeep();
+		}
+	}
+
+	public void EndTurn() {
+		StartTurn();
+	}
+	public void ClickCard(CardScript card) {
+		kings[0].DrawCard(card);
+		int id = cardsOnTable.IndexOf(card);
+		switch(id)
+		{
+		case 0:
+			kings[1].DrawCard(cardsOnTable[1]);
+			kings[2].DrawCard(cardsOnTable[2]);
+			break;
+		case 1:
+			kings[0].DrawCard(cardsOnTable[0]);
+			kings[2].DrawCard(cardsOnTable[2]);
+			break;
+		case 2:
+			kings[0].DrawCard(cardsOnTable[0]);
+			kings[1].DrawCard(cardsOnTable[1]);
+			break;
+		}
+		EndTurn ();
+	}
+
+	private void NewCards() {
+		for(int i = 0; i < 3; i++) {
+			CardScript c = possibleCards[Random.Range (0, possibleCards.Count)];
+			cardsOnTable[i] = c;
+		}
+	}
 }
 
 [System.Serializable]
@@ -24,5 +82,8 @@ public class King {
 			rauha += cs.rauha;
 			suosio += cs.suosio;
 		}
+	}
+	public void DrawCard(CardScript card) {
+		cards.Add(card);
 	}
 }
