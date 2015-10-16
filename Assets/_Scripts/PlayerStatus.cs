8c81f5f05245518cbfc5cf414c38254cced255c1
Jagulars <sakari.lukkaroinen@hotmail.com>
2015-10-16 23:56:42 +0300

    ihaa

--- Assets/_Scripts/PlayerStatus.cs
+++ Assets/_Scripts/PlayerStatus.cs
@@ -1,17 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerStatus : MonoBehaviour {
 
-    int wealth;
-    int favorKing;
-    int favorPeople;
-    int peace;
-    int military;
+    public GameObject wealthText;
+    public GameObject kingText;
+    public GameObject peopleText;
+    public GameObject peaceText;
+    public int wealth;
+    public int favorKing;
+    public int favorPeople;
+    public int peace;
+    Text wealthNumber;
+    Text kingNumber;
+    Text peopleNumber;
+    Text peaceNumber;
     
 
-    void EndOfTurn()
+    //int military;
+
+    void Start()
     {
-        peace = favorPeople + military;
+        
+        wealth = 50;
+        favorKing = 50;
+        favorPeople = 50;
+        //military = 10;
+        wealthNumber = wealthText.GetComponent<Text>();
+        kingNumber = kingText.GetComponent<Text>();
+        peopleNumber = peopleText.GetComponent<Text>();
+        peaceNumber = peopleText.GetComponent<Text>();
+    }
+    
+
+    public void EndOfTurn()
+    {
+        peace = favorPeople;// + military;
+        wealthNumber.text = wealth.ToString();
+        kingNumber.text = favorKing.ToString();
+        peopleNumber.text = favorPeople.ToString();
+        peaceNumber.text = peace.ToString();
+        
+        //Military upkeep:
+        //wealth -= military * 10;
     }
 }
