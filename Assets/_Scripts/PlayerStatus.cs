1b69591acfb3b51f19013bec4aa1f99bd3ed69a7
Jagulars <sakari.lukkaroinen@hotmail.com>
2015-10-17 12:05:35 +0300

    yuyu

--- Assets/_Scripts/PlayerStatus.cs
+++ Assets/_Scripts/PlayerStatus.cs
@@ -4,16 +4,17 @@ using System.Collections;
 
 public class PlayerStatus : MonoBehaviour {
 
+    public TurnController turnCtrl;
     public GameObject wealthText;
-    public GameObject kingText;
+    //public GameObject kingText;
     public GameObject peopleText;
     public GameObject peaceText;
     public int wealth;
-    public int favorKing;
+    //public int favorKing;
     public int favorPeople;
     public int peace;
     Text wealthNumber;
-    Text kingNumber;
+    //Text kingNumber;
     Text peopleNumber;
     Text peaceNumber;
     
@@ -22,26 +23,30 @@ public class PlayerStatus : MonoBehaviour {
 
     void Start()
     {
-        
+        turnCtrl.GetComponent<TurnController>();
         wealth = 50;
-        favorKing = 50;
+        //favorKing = 50;
         favorPeople = 50;
         //military = 10;
         wealthNumber = wealthText.GetComponent<Text>();
-        kingNumber = kingText.GetComponent<Text>();
+        //kingNumber = kingText.GetComponent<Text>();
         peopleNumber = peopleText.GetComponent<Text>();
         peaceNumber = peopleText.GetComponent<Text>();
     }
-    
+
 
     public void EndOfTurn()
     {
+        wealth = (int)turnCtrl.kings[0].raha;
+        favorPeople = (int)turnCtrl.kings[0].suosio;
+        peace = (int)turnCtrl.kings[0].rauha;
         peace = favorPeople;// + military;
         wealthNumber.text = wealth.ToString();
-        kingNumber.text = favorKing.ToString();
+        //kingNumber.text = favorKing.ToString();
         peopleNumber.text = favorPeople.ToString();
         peaceNumber.text = peace.ToString();
         
+
         //Military upkeep:
         //wealth -= military * 10;
     }
